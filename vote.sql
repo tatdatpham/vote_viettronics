04ab781d92424087593db78a96043fe4a9a20e11
Tất Đạt Phạm <tatdat171@gmail.com>
2014-09-28 23:26:12 +0700

    Update database
    Update database

--- vote.sql
+++ vote.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50616
 File Encoding         : 65001
 
-Date: 2014-09-21 09:31:16
+Date: 2014-09-28 23:25:46
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -21,7 +21,7 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `account`;
 CREATE TABLE `account` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(50) NOT NULL,
+  `account` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `fullname` varchar(50) NOT NULL,
   `avatar` varchar(50) DEFAULT 'avatar-default.jpg',
@@ -29,21 +29,20 @@ CREATE TABLE `account` (
   `teaching` text NOT NULL,
   `introduced` text NOT NULL,
   `status` int(5) DEFAULT '1',
-  PRIMARY KEY (`id`),
-  KEY `unit_id` (`unit_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of account
 -- ----------------------------
-INSERT INTO `account` VALUES ('1', 'admin@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'Phạm Tất Đạt', 'avatar-1.jpg', '1', 'Phát triển ứng dụng web', 'Hôm nay, xin chia sẻ với anh em hình ảnh ngôi sao thời trang Hạ Anh cùng tai nghe độ dán da trang trí từ Khắc Tên. Bộ ảnh này được nhiếp ảnh gia ProK thực hiện. Tai nghe dùng trong loạt này là chiếc Harman Kardon CL màu đen và được thiết kế bên Khắc Tên trang trí lại với màu vàng và màu tím. Chẳng biết nói gì hơn, thôi anh em coi hình đi. Nhiếp Ảnh Gia: ProK Đặng Quốc Chương Mẫu: Hạ Anh, Ngôi Sao Thời Trang Máy: Tai nghe trang trí da từ...', '3');
-INSERT INTO `account` VALUES ('2', 'manhnb@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'Bùi Tiến Mạnh', 'avatar-2.jpg', '2', 'Phát triển ứng dụng web', 'Phân khúc xe hatchback cao cấp chỉ thật sự nóng lên vào giữa năm ngoái, khi Mercedes Benz khuấy động thị trường Việt Nam với việc lần đầu ra mắt chiếc A-class thế hệ thứ 3. Ngay sau đó, BMW cũng kịp thời ra mắt chiếc BMW 116i để cạnh tranh với đối thủ A200 thấp nhất trong dòng A-class của Mercedes.', '2');
-INSERT INTO `account` VALUES ('3', 'ngant@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'Nguyễn Thị Ngân', 'avatar-3.jpg', '1', 'Phát triển ứng dụng web', 'Toshiba mới đây thông báo hãng sẽ thúc đẩy quá trình tái cấu trúc ngành hàng PC của mình nhằm tập trung hơn vào các sản phẩm dành cho doanh nghiệp (Business to Business - B2B), đồng thời thu hẹp phân khúc hướng đến người tiêu dùng (Business to Consumer - B2C)', '2');
-INSERT INTO `account` VALUES ('4', 'thaont@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'Nguyễn Thị Thảo', 'avatar-4.jpg', '1', 'Không gì cả', 'Trong bài đánh giá hôm nay, chúng ta hãy cùng điểm qua những ưu nhược điểm và hiệu năng của chiếc máy tính 2 trong 1 ASUS Transformer Book T200. Đây là phiên bản nâng cấp của chiếc Transformer Book T100 năm ngoái.', '2');
-INSERT INTO `account` VALUES ('5', 'thongbm@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'Bùi Minh Thông', 'avatar-5.jpg', '1', 'Công nghệ phần mềm', 'Sở hữu một chiếc ô tô ở Việt Nam là cả một vấn đề không hề đơn giản, vì ngoài việc mua xe với mức giá cao do chịu nhiều loại thuế thì trong quá trình sử dụng nó chúng ta còn phải gánh thêm rất nhiều chi phí nuôi xe khác', '2');
-INSERT INTO `account` VALUES ('6', 'kunny171@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'Phạm Tất Đạt', 'avatar-6.jpg', '1', 'Công nghệ phần mềm', 'Chiếc tablet Kindle Fire HDX 8.9\" của Amazon năm nay đã được nâng cấp mới với chip xử lý Snapdragon 805 nhanh hơn, cải tiến công nghệ âm thanh Dolby Atmos, chạy trên hệ điều hành Fire OS 4 mới nhất, pin 12 tiếng và có luôn tính năng tìm thông tin đồ vật Firefly vốn trước đây chỉ có trên điện thoại Fire của hãng này.', '2');
-INSERT INTO `account` VALUES ('7', 'sennt@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'Nguyễn Thị Sen', 'avatar-7.jpg', '3', 'Mạng máy tính', 'Đúng như tên gọi của nó, Fire HD Kids Edition là một chiếc tablet dành riêng cho trẻ em và Amazon đã rất cố gắng làm cho nó phù hợp nhất có thể đối với lứa tuổi thiếu nhi. Mặc dù nó mắc hơn bản Fire HD thường 50 USD nhưng bù lại các bậc cha mẹ sẽ yên tâm hơn nhiều vì Amazon chấp nhận bảo hành tất cả mọi sự cố hỏng hóc trong vòng 2 năm và trả lại máy mới.', '2');
-INSERT INTO `account` VALUES ('8', 'test@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'test', 'avatar-8.jpg', '3', 'Chế tạo máy', 'Khoa CNTT', '1');
+INSERT INTO `account` VALUES ('1', 'admin', 'e10adc3949ba59abbe56e057f20f883e', 'Phạm Tất Đạt', 'avatar-1.jpg', '1', 'Phát triển ứng dụng web', 'Hôm nay, xin chia sẻ với anh em hình ảnh ngôi sao thời trang Hạ Anh cùng tai nghe độ dán da trang trí từ Khắc Tên. Bộ ảnh này được nhiếp ảnh gia ProK thực hiện. Tai nghe dùng trong loạt này là chiếc Harman Kardon CL màu đen và được thiết kế bên Khắc Tên trang trí lại với màu vàng và màu tím. Chẳng biết nói gì hơn, thôi anh em coi hình đi. Nhiếp Ảnh Gia: ProK Đặng Quốc Chương Mẫu: Hạ Anh, Ngôi Sao Thời Trang Máy: Tai nghe trang trí da từ...', '3');
+INSERT INTO `account` VALUES ('2', 'manhnb', 'e10adc3949ba59abbe56e057f20f883e', 'Bùi Tiến Mạnh', 'avatar-2.jpg', '2', 'Phát triển ứng dụng web', 'Phân khúc xe hatchback cao cấp chỉ thật sự nóng lên vào giữa năm ngoái, khi Mercedes Benz khuấy động thị trường Việt Nam với việc lần đầu ra mắt chiếc A-class thế hệ thứ 3. Ngay sau đó, BMW cũng kịp thời ra mắt chiếc BMW 116i để cạnh tranh với đối thủ A200 thấp nhất trong dòng A-class của Mercedes.', '2');
+INSERT INTO `account` VALUES ('3', 'ngant', 'e10adc3949ba59abbe56e057f20f883e', 'Nguyễn Thị Ngân', 'avatar-3.jpg', '1', 'Phát triển ứng dụng web', 'Toshiba mới đây thông báo hãng sẽ thúc đẩy quá trình tái cấu trúc ngành hàng PC của mình nhằm tập trung hơn vào các sản phẩm dành cho doanh nghiệp (Business to Business - B2B), đồng thời thu hẹp phân khúc hướng đến người tiêu dùng (Business to Consumer - B2C)', '2');
+INSERT INTO `account` VALUES ('4', 'thaont', 'e10adc3949ba59abbe56e057f20f883e', 'Nguyễn Thị Thảo', 'avatar-4.jpg', '1', 'Không gì cả', 'Trong bài đánh giá hôm nay, chúng ta hãy cùng điểm qua những ưu nhược điểm và hiệu năng của chiếc máy tính 2 trong 1 ASUS Transformer Book T200. Đây là phiên bản nâng cấp của chiếc Transformer Book T100 năm ngoái.', '2');
+INSERT INTO `account` VALUES ('5', 'thongbm', 'e10adc3949ba59abbe56e057f20f883e', 'Bùi Minh Thông', 'avatar-5.jpg', '1', 'Công nghệ phần mềm', 'Sở hữu một chiếc ô tô ở Việt Nam là cả một vấn đề không hề đơn giản, vì ngoài việc mua xe với mức giá cao do chịu nhiều loại thuế thì trong quá trình sử dụng nó chúng ta còn phải gánh thêm rất nhiều chi phí nuôi xe khác', '2');
+INSERT INTO `account` VALUES ('6', 'kunny171', 'e10adc3949ba59abbe56e057f20f883e', 'Phạm Tất Đạt', 'avatar-6.jpg', '1', 'Công nghệ phần mềm', 'Chiếc tablet Kindle Fire HDX 8.9\" của Amazon năm nay đã được nâng cấp mới với chip xử lý Snapdragon 805 nhanh hơn, cải tiến công nghệ âm thanh Dolby Atmos, chạy trên hệ điều hành Fire OS 4 mới nhất, pin 12 tiếng và có luôn tính năng tìm thông tin đồ vật Firefly vốn trước đây chỉ có trên điện thoại Fire của hãng này.', '2');
+INSERT INTO `account` VALUES ('7', 'sennt', 'e10adc3949ba59abbe56e057f20f883e', 'Nguyễn Thị Sen', 'avatar-7.jpg', '3', 'Mạng máy tính', 'Đúng như tên gọi của nó, Fire HD Kids Edition là một chiếc tablet dành riêng cho trẻ em và Amazon đã rất cố gắng làm cho nó phù hợp nhất có thể đối với lứa tuổi thiếu nhi. Mặc dù nó mắc hơn bản Fire HD thường 50 USD nhưng bù lại các bậc cha mẹ sẽ yên tâm hơn nhiều vì Amazon chấp nhận bảo hành tất cả mọi sự cố hỏng hóc trong vòng 2 năm và trả lại máy mới.', '2');
+INSERT INTO `account` VALUES ('8', 'test', 'e10adc3949ba59abbe56e057f20f883e', 'test', 'avatar-8.jpg', '3', 'Chế tạo máy', 'Khoa CNTT', '1');
 
 -- ----------------------------
 -- Table structure for guide
@@ -93,7 +92,7 @@ CREATE TABLE `vote` (
   PRIMARY KEY (`id`),
   KEY `account_id1` (`account_id`),
   CONSTRAINT `fk_user_vote` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of vote
@@ -114,12 +113,12 @@ CREATE TABLE `vote_time` (
   `timeend` datetime NOT NULL,
   `active` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of vote_time
 -- ----------------------------
-INSERT INTO `vote_time` VALUES ('1', '2014-09-20 22:02:20', '2014-09-21 19:02:29', '0');
+INSERT INTO `vote_time` VALUES ('1', '2014-09-20 22:02:20', '2014-09-27 19:02:29', '1');
 
 -- ----------------------------
 -- View structure for account_info
